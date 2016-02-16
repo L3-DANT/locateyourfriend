d6dd00db1e765a598608b389d01feef2faf01d1b
Brandon Caurel <caurelbrandon@hotmail.fr>
2016-02-16 19:41:53 +0100

    Implémentation des logs, modification du gitignore; Les logs s'ecrive dans un dossier au sein du projet nommé logs_locateyourfriend (ajout dans le gitignore) qu'il vous faut crée pour que les logs soit écrit

---
+++ src/main/java/com/locateyourfriend/logger/MyLogger.java
@@ -0,0 +1,44 @@
+package com.locateyourfriend.logger;
+
+import java.io.IOException;
+import java.util.logging.*;
+import java.text.SimpleDateFormat; 
+import java.util.Date; 
+
+
+public class MyLogger {
+	
+	static private FileHandler fh = null; //C'est un pointeur vers notre fichier log
+	static private SimpleDateFormat f = new SimpleDateFormat("dd_MM_yy_H'h'm'm's's'");//On formate la date : 01/01/16 par exemple 
+	static private Date d = new Date(); //Ici on recupere la date du systeme pour l'incorporer au nom de notre fichier log
+	
+	public MyLogger(){ 
+		
+	}	
+	
+	public static void setup(){
+		// Le logger correspond au journal de log qui va traiter nos logs
+		Logger logger = Logger.getLogger("MonJournalDeLogs");
+			   	
+			try {
+				//Ici on indique au FileHandler le nom et l'emplacement du fichier log
+				//Il le creera si il n'existe pas
+				fh = new FileHandler("logs_locateyourfriend/log"+f.format(d)+".log"); 
+				logger.addHandler(fh);
+				
+				//On set le format du fichier log (Choix XML ou simple), dans notre cas, simple
+				fh.setFormatter(new SimpleFormatter());
+//				fh.close();
+				
+			} catch (SecurityException e) {
+				
+				e.printStackTrace();
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+
+	}
+
+}
