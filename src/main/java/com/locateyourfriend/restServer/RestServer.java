601f46787526ef5357782c6350e65a601f9172fb
Brandon Caurel <brandon@MacBook-Pro-de-Brandon.local>
2016-03-17 11:18:36 +0100

    Clean des config serveur + connexion entre appli ios et serveur fonctionnelle

--- src/main/java/com/locateyourfriend/restServer/RestServer.java
+++ src/main/java/com/locateyourfriend/restServer/RestServer.java
@@ -1,35 +1,38 @@
 package com.locateyourfriend.restServer;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@Path("/bienvenue")
 public class RestServer{
 	
-	@Inject
-	Logger logger;
+	
+	//Logger logger;
+	
+	
 	
 	@GET
-	@Path("/bienvenue")
 	public String bienvenue()
 	{
-		String message = "Connection au serveur établie !";
-		logger.log(Level.INFO, message);
+		String message = "Connection au serveur ï¿½tablie !";
+		//logger.log(Level.INFO, message);
 		return message;
 	}
 	
-	@GET
+	@POST
 	@Path("/bienvenueJSON")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String bienvenueJSON()
-	{
-		String message = "Connection au serveur établie !";
-		logger.log(Level.INFO, message);
-		return message;
+	@Consumes(MediaType.APPLICATION_JSON) 
+	public String getMessage(@QueryParam("email") String email, @QueryParam("password") String password ){
+		return "{Connection au serveur ï¿½tablie !}";
 	}
 
 }
